792c543f077b6f79b484283e6ba1f14c5922cf3a
raffinugraha <154036831+raffinugraha@users.noreply.github.com>
2023-12-17 11:11:49 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const sideNav = document.querySelectorAll(".sidenav");
+M.Sidenav.init(sideNav);
+
+const slider = document.querySelectorAll(".slider");
+M.Slider.init(slider, {
+  indicators: false,
+  height: 600,
+  transition: 1000,
+  interval: 3000,
+});
+
+const parallax = document.querySelectorAll(".parallax");
+M.Parallax.init(parallax);
+
+const materialboxed = document.querySelectorAll(".materialboxed");
+M.Materialbox.init(materialboxed);
+
+const scrollspy = document.querySelectorAll(".scrollspy");
+M.ScrollSpy.init(scrollspy,{
+  scrollOffset: 50
+});
